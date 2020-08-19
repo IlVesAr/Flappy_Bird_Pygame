871dde3d1abed32fad434a027f970b44fe0f2e3f
unknown <ilian2005tttt@gmail.com>
2020-08-19 16:12:59 +0300

    Playable Game

--- flappy_bird.py
+++ flappy_bird.py
@@ -9,9 +9,9 @@ speed = 3
 width, height = 800, 800
 
 kol_dol = [pg_plus.Kvadrat(800, random.randrange(20 + vis, height - 20), 60, 1000, 0, speed, pg_plus.color.green),
-           pg_plus.Kvadrat(1050, random.randrange(20 + vis, height - 20), 60, 1000, 0, speed, pg_plus.color.green),
-           pg_plus.Kvadrat(1300, random.randrange(20 + vis, height - 20), 60, 1000, 0, speed, pg_plus.color.green),
-           pg_plus.Kvadrat(1550, random.randrange(20 + vis, height - 20), 60, 1000, 0, speed, pg_plus.color.green)]
+           pg_plus.Kvadrat(800 + dis, random.randrange(20 + vis, height - 20), 60, 1000, 0, speed, pg_plus.color.green),
+           pg_plus.Kvadrat(800 + dis * 2, random.randrange(20 + vis, height - 20), 60, 1000, 0, speed, pg_plus.color.green),
+           pg_plus.Kvadrat(800 + dis * 3, random.randrange(20 + vis, height - 20), 60, 1000, 0, speed, pg_plus.color.green)]
 
 kol_gor = [pg_plus.Kvadrat(kol_dol[0].x, kol_dol[0].y - kol_dol[0].h - vis, 60, 1000, 0, speed, pg_plus.color.green),
            pg_plus.Kvadrat(kol_dol[1].x, kol_dol[1].y - kol_dol[1].h - vis, 60, 1000, 0, speed, pg_plus.color.green),
@@ -47,6 +47,22 @@ def Grav(g, V, fps):
     return V
 
 
+def Die():
+    global kol_dol, kol_gor
+    global bird
+    global run
+    for i3 in range(0, 4):
+        if kol_dol[i3].x <= bird.x + bird.w <= kol_dol[i3].x + kol_dol[i3].w and kol_dol[i3].y <= bird.y + bird.h <= kol_dol[i3].y + kol_dol[i3].h:
+            run = False
+        if kol_dol[i3].x <= bird.x <= kol_dol[i3].x + kol_dol[i3].w and kol_dol[i3].y <= bird.y + bird.h <= kol_dol[i3].y + kol_dol[i3].h:
+            run = False
+
+        if kol_gor[i3].x <= bird.x + bird.w <= kol_gor[i3].x + kol_gor[i3].w and bird.y <= kol_gor[i3].y + kol_gor[i3].h:
+            run = False
+        if kol_gor[i3].x <= bird.x <= kol_gor[i3].x + kol_gor[i3].w and bird.y <= kol_gor[i3].y + kol_gor[i3].h:
+            run = False
+
+
 pygame.init()
 
 win = pygame.display.set_mode((width, height))
@@ -78,6 +94,7 @@ while run:
     if key[pygame.K_SPACE]:
         V = 8
 
+    Die()
     Move()
     DrAw(win)
 
